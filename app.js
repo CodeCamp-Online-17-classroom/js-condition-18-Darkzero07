1c4dd953a822972b9996b98c2b9c8649eedbc3b6
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 12:31:16 +0700

    js lab 35

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-// write code here
+let login = prompt('Enter username');
+let message;
+
+message = (login == 'Employee') ? alert('Hello') : (login == 'Director') ? alert( 'Greetings') : (login == '') ? alert('No login') : alert("")
